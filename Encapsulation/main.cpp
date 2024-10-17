6d9824fc9811f4becf75b5337d918ec36b7485b9
Patania <ivaniatavaressilva@gmail.com>
2024-10-17 20:37:28 +0200

    njdksqhjk

--- Encapsulation/main.cpp
+++ Encapsulation/main.cpp
@@ -1,9 +1,7 @@
 #include "main.h"
-#include "ballManager.h"
 
 using namespace std;
 
-<<<<<<< HEAD
 int main(int argc, char* argv[])
 {
 	SDL_Window* window = NULL;
@@ -12,32 +10,12 @@ int main(int argc, char* argv[])
 	Uint32 startTicks = SDL_GetTicks64();
 
 	Ball ball;
-=======
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
-
-
-Ball ball (0,0,1,1);
-int quit = 0;
-
-Uint32 startTicks = SDL_GetTicks64();
-
-std::vector<Ball> balls = {};
-
-int main(int argc, char* argv[]) 
-{
-
-	if (createWindow(&window, &renderer)!= 0) {
-		return -1;
-	}
-	while (!quit) {
->>>>>>> 05eb1c21f302232d011e3e80261224e994e47730
 
 	int quit = 0;
 
 
 	std::vector<Ball> ballArray = {};
-
+	
 	if (SDLInit(&window, &renderer) !=0) {return -1;}
 
 	std::srand(static_cast<unsigned int>(std::time(0)));
@@ -50,42 +28,29 @@ int main(int argc, char* argv[])
 		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 		SDL_RenderClear(renderer);
 
-<<<<<<< HEAD
-=======
-		Uint32 timeElapsed = SDL_GetTicks64() - startTicks;
 
 		if (timeElapsed >= 1000)
 		{
-			ball.RandomValuesInit();
-			ball.Spawn();
-			balls.emplace_back(ball);
+			ball.Init(ballArray, ball);
 			startTicks = SDL_GetTicks64();
 
 		}
-
-		if (balls.size() >= 20)
+		if (ballArray.size() >= MAX_BALL_AMOUNT)
 		{
-			balls[0].SetVisibility();
-			balls[1].SetVisibility();
-			balls.erase(balls.begin());
-			balls.shrink_to_fit();
+			ball.Delete(ballArray);
 		}
 
-		for (int i = 0; i < balls.size(); i++) {
->>>>>>> 05eb1c21f302232d011e3e80261224e994e47730
-
-		ball.Game(timeElapsed, startTicks, ballArray, ball, renderer);
+		for (int i = 0; i < ballArray.size(); i++)
+		{
+			ballArray[i].Update(ballArray, renderer);
+		}
+		//ball.Game(timeElapsed, startTicks, ballArray, ball, renderer);
 
 		SDL_RenderPresent(renderer);
-		//SDL_Delay(10);
+		SDL_Delay(10);
 	}
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
-}
-
-void gameManager() 
-{
-
 }
